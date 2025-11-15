c7a7df337de9ee4860d2dfcecc5da1ce8bac7e0c
Shivan Sagar Gupta <guptajisagar414@gmail.com>
2025-11-15 22:40:35 +0530

    ssf

--- backend/app.js
+++ backend/app.js
@@ -84,6 +84,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:5500",
   "http://localhost:5173",
+  "https://library-system-navy-two.vercel.app",
   "https://library-system-quok999il-sagarshivan123s-projects.vercel.app",
   "https://library-system-git-main-sagarshivan123s-projects.vercel.app"
 ];
